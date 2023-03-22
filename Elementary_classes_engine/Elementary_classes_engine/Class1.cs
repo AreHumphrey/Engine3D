04537cf9da71270e49c3f67605b4119efc34706d
Dina <lutdina04@yandex.ru>
2023-03-23 09:18:43 +1000

    Add files via upload

---
+++ Elementary_classes_engine/Elementary_classes_engine/Class1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime.Intrinsics;
+using System.Threading;
+
+namespace Elementary_classes_engine
+{
+    public class Program
+    {
+        public class point
+        {
+            
+            public double x { get; private set; }
+            public double y { get; private set; }
+            public double z { get; private set; }
+            public point(double x, double y, double z)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+            //Перегрузка операторов ( + - * / ) + расстояние
+            public static double distance(point a, point b)
+            {
+                return Math.Sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) + (a.z - b.z) * (a.z - b.z));
+            }
+            public static point operator +(point a, point b)
+            {
+                return new point(a.x + b.x, a.y + b.y, a.z + b.z);
+            }
+            public static point operator -(point a, point b)
+            {
+                return new point(a.x - b.x, a.y - b.y, a.z - b.z);
+            }
+            public static point operator *(point a, point b)
+            {
+                return new point(a.x * b.x, a.y * b.y, a.z * b.z);
+            }
+            public static point operator /(point a, point b)
+            {
+                return new point(a.x / b.x, a.y / b.y, a.z / b.z);
+            }
+           
+
+            public static point operator *(point a, double Element)
+            {
+                return new point(a.x * Element, a.y * Element, a.z * Element);
+            }
+            public static point operator *(double Element, point b)
+            {
+                return new point(b.x * Element, b.y * Element, b.z * Element);
+            }
+            public static point operator /(point a, double Element)
+            {
+                return new point(Element / a.x, Element / a.y, Element / a.z);
+            }
+        }
+        public class Vector
+        {
+            public point pt;
+
+            public Vector(point pt)
+            {
+                this.pt = pt;
+
+            }
+
+            public static double LenVector(Vector v1)
+            {
+                return Math.Sqrt(v1.pt.x * v1.pt.x + v1.pt.y * v1.pt.y + v1.pt.z * v1.pt.z);
+            }
+
+
+            //Перегрузка операторов ( + - * / ) + расстояние
+            public static Vector operator +(Vector a, Vector b)
+
+            {
+                return new Vector(new point(a.pt.x + b.pt.x, a.pt.y + b.pt.y, a.pt.z + b.pt.z));
+            }
+            public static Vector operator -(Vector a, Vector b)
+            {
+                return new Vector(new point(a.pt.x - b.pt.x, a.pt.y - b.pt.y, a.pt.z - b.pt.z));
+            }
+            public static Vector operator *(Vector a, Vector b)
+            {
+                return new Vector(new point(a.pt.x * b.pt.x, a.pt.y * b.pt.y, a.pt.z * b.pt.z));
+            }
+            public static Vector operator ^(Vector a, Vector b)
+            {
+                return new Vector(new point(a.pt.y * b.pt.z - a.pt.z * b.pt.z, a.pt.z * b.pt.x - a.pt.x * b.pt.z, a.pt.x * b.pt.y - a.pt.y * b.pt.x));
+            }
+            public static Vector operator /(Vector a, Vector b)
+            {
+                return new Vector(new point(a.pt.x / b.pt.x, a.pt.y / b.pt.y, a.pt.z / b.pt.z));
+            }
+            public static Vector operator *(double Element, Vector b)
+            {
+                return new Vector(new point(Element * b.pt.x, Element * b.pt.y, Element * b.pt.z));
+            }
+            public static Vector operator *(Vector a, double Element)
+            {
+                return new Vector(new point(Element * a.pt.x, Element * a.pt.y, Element * a.pt.z));
+            }
+            public static Vector operator /(Vector a, double Element)
+            {
+                return new Vector(new point(a.pt.x / Element, a.pt.y / Element, a.pt.z / Element));
+            }
+            public static Vector operator /(double Element, Vector b)
+            {
+                return new Vector(new point(Element / b.pt.x, Element / b.pt.y, Element / b.pt.z));
+            }
+
+
+        }
+        public class VectorSpace
+        {
+            public point Initpt;
+            public Vector Dir1;
+            public Vector Dir2;
+            public Vector Dir3;
+
+            public VectorSpace(point InitPt, Vector Dir1, Vector Dir2, Vector Dir3)
+            {
+                this.Initpt = InitPt;
+                this.Dir1 = VectorBasis(Dir1);
+                this.Dir2 = VectorBasis(Dir1);
+                this.Dir3 = VectorBasis(Dir1);
+            }
+
+            public static Vector VectorBasis(Vector vect)
+            {
+                double VectorsLen = Vector.LenVector(vect);
+                return vect / VectorsLen;
+            }
+        }
+        public class Camera
+        {
+            public double Width = 70;
+            public double Height = 50;
+            public double Fov; //d +
+            public double VFov; //d + 
+            public point Look_at;
+            public double Look_dir;
+            public double DrawDistance;
+            public Camera(point Pos, point Look_at, double Look_dir, double Fov, double DrawDistance, double vFov)
+            {
+                this.Look_at = Look_at;
+                this.Look_dir = Look_dir;
+                this.Fov = Fov;
+                this.VFov = vFov;
+                vFov = Fov * (Height / Width);
+                this.DrawDistance = DrawDistance;
+
+            }
+        }
+        public class Object
+        {
+            public point Position;
+            public Vector Rotation; //пока оставить
+            public Object(point Position, Vector Rotation)
+            {
+                this.Position = Position;
+                this.Rotation = Rotation;
+            }
+
+
+        }
+
+
+
+    }
+
+
+}
---
+++ Elementary_classes_engine/TestForMyClass/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using static Elementary_classes_engine.Program;
+
+namespace Tests_for_Class
+{
+    public class Program
+    {
+        static void Main()
+        {
+            point Pt1 = new point(1, 2, 5);
+            point Pt2 = new point(-1, 4, 6);
+            point Pt = new point(0, 0, -1);
+            Console.WriteLine($"Pt1: ({Pt1.x}, {Pt1.y}, {Pt1.z}) \nPt2: ({Pt2.x}, {Pt2.y}, {Pt2.z})");
+
+            point Pt0 = Pt + Pt2; // (0, 0, -1) + (-1, 4, 6) Pt Pt2
+            Console.WriteLine($"Pt0: ({Pt0.x}, {Pt0.y}, {Pt0.z})"); // (-1, 4, 5) Pt0
+            point Pt3 = Pt0 - Pt1; // (-1, 4, 5) - (1, 2, 5)
+            Console.WriteLine($"Pt3: ({Pt3.x}, {Pt3.y}, {Pt3.z})"); //(-2, 2, 0) Pt3
+            double d = 9;
+            point Pt4 = Pt3 * d;
+            Console.WriteLine($"Pt4: ({Pt4.x}, {Pt4.y}, {Pt4.z})"); //Pt4 (-18, 18, 0)
+            point Pt5 = d * Pt3;
+            Console.WriteLine($"Pt5: ({Pt5.x}, {Pt5.y}, {Pt5.z})"); //Pt5 (-18, 18, 0)
+            point Pt6 = new point(2, 3, -2);
+            point Pt7 = new point(5, 5, -25);
+            Vector V1 = new Vector(Pt6);
+            Vector V2 = new Vector(Pt7);
+            Vector V3 = V1 + V2; //(2, 3, -2) + (5, 5, -25)
+            Vector V4 = V1 - V2; //(2, 3, -2) - (5, 5, -25)
+            Vector V5 = V1 * d; //(2, 3, -2) * 9
+            Console.WriteLine($"V3: ({V3.pt.x}, {V3.pt.y}, {V3.pt.z})"); //V3: (7, 8, -27)
+            Console.WriteLine($"V4: ({V4.pt.x}, {V4.pt.y}, {V4.pt.z})"); //V4: (-3, -2, 23)
+            Console.WriteLine($"V5: ({V5.pt.x}, {V5.pt.y}, {V5.pt.z})"); //V5: (18, 27, -18)
+            Vector V6 = V1 ^ V2;
+            Console.WriteLine($"V6: ({V6.pt.x}, {V6.pt.y}, {V6.pt.z})"); //V6: (-125, 40, -5)
+            double LenOfVectorV6 = Vector.LenVector(V6); //(Значение с онлайн калькулятора ≈ 131.33925536563697) мое Len: 131,33925536563697
+            double LenOfVectorV5= Vector.LenVector(V5); // Len: 37,107950630558946 (≈ 37.107950630558946)
+            double LenOfVectorV4 = Vector.LenVector(V4); //Len: 23,280893453645632 (≈ 23.280893453645632)
+            double LenOfVectorV3 = Vector.LenVector(V3); //Len: 29,017236257093817 (≈ 29.017236257093817)
+            Console.WriteLine($"Len V6: {LenOfVectorV6}");
+            Console.WriteLine($"Len V5: {LenOfVectorV5}");
+            Console.WriteLine($"Len V4: {LenOfVectorV4}");
+            Console.WriteLine($"Len V3: {LenOfVectorV3}");
+
+
+
+
+        }
+    }
+}
+
