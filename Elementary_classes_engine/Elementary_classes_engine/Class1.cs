563a2fca32f3f1817cff925e44114665415e9ee5
Dina <lutdina04@yandex.ru>
2023-04-05 00:07:53 +1000

    Update Class1.cs

--- Elementary_classes_engine/Elementary_classes_engine/Class1.cs
+++ Elementary_classes_engine/Elementary_classes_engine/Class1.cs
@@ -10,7 +10,7 @@ namespace Elementary_classes_engine
     {
         public class point
         {
-            
+
             public double x { get; private set; }
             public double y { get; private set; }
             public double z { get; private set; }
@@ -134,12 +134,12 @@ namespace Elementary_classes_engine
                 return vect / VectorsLen;
             }
         }
-        public class Camera
+        public abstract class Camera
         {
             public double Width = 70;
             public double Height = 50;
-            public double Fov; //d +
-            public double VFov; //d + 
+            public double Fov; 
+            public double VFov; 
             public point Look_at;
             public double Look_dir;
             public double DrawDistance;
@@ -153,12 +153,14 @@ namespace Elementary_classes_engine
                 this.DrawDistance = DrawDistance;
 
             }
+            public abstract double Func();// Пустить во все стороны от камеры лучи (необходим подсчет)
+            public abstract double DistanceObject(Object obj);
         }
         public static double Sum(double a, double b)
         {
             return a + b;
         }
-        public class Object
+        public abstract class Object
         {
             public point Position;
             public Vector Rotation; //пока оставить
@@ -168,42 +170,127 @@ namespace Elementary_classes_engine
                 this.Rotation = Rotation;
             }
 
-            public static void Contains(point Pt)
+            public abstract bool Contains(point Pt); //Лежит ли точка ДОБАВИТЬ (bool)
+
+            public abstract bool Intersect(Ray)
             {
-                //Лежит ли точка ДОБАВИТЬ (bool)
+                //Пересечение луча ДОБАВИТЬ
+                // Заглушка для определения точки пересечения прямой с объектом
             }
-            public static void Intersect(Vector V)
+            // Заглушка для определения ближайшей точки из набора
+            public virtual point getNearestPoint(point[] pts)
             {
-                 //Пересечение луча ДОБАВИТЬ
+                point nearestPoint = null;
+                double closestDistance = double.PositiveInfinity;
+                foreach (point p in pts)
+                {
+                    double distance = distanceTo(point);
+                    if (distance < closestDistance)
+                    {
+                        nearestPoint = p;
+                        closestDistance = distance;
+                    }
+                }
+                return nearestPoint;
+            }
+            protected double distanceTo(Point point)
+            {
+                double dx = point.x - position.x;
+                double dy = point.y - position.y;
+                double dz = point.z - position.z;
+                return Math.Sqrt(dx * dx + dy * dy + dz * dz);
             }
 
-          
         }
+       
         public class Plane : Object // Плоскость наследуется от объекта 
         {
-            public Plane(point Position, Vector Rotation) : base(Position, Rotation){}
-            public static void contains(point Pt)
+            public ParametersPlane parameters;
+
+            public Plane(point position, Vector rotation)
             {
-                //Лежит ли точка ДОБАВИТЬ (bool)
+                this.position = position;
+                this.rotation = rotation;
+                Parameters = new ParametersPlane();
             }
-            public static void intersect(Vector V)
+
+            public override point intersect(Vector V, point pt)
             {
-                // Точка Пересечения камеры и объекта ДОБАВИТЬ
+                double denominator = parameters.CalculateValue(direction);
+                if (denominator == 0) // прямая параллельна плоскости или лежит в плоскости
+                {
+                    if (Contains(point)) // прямая лежит в плоскости
+                    {
+                        return point; // любая точка прямой будет являться точкой пересечения
+                    }
+                    else
+                    {
+                        return null; // прямая параллельна плоскости и не пересекает ее
+                    }
+                }
+                else // прямая пересекает плоскость
+                {
+                    double numerator = parameters.CalculateValue(position) - parameters.CalculateValue(point);
+                    double distance = numerator / denominator;
+                    return new Point(point.x + direction.x * distance, point.y + direction.y * distance, point.z + direction.z * distance);
+                }
+               
+            }
+
+            public override bool Contains(point Pt)
+            {
+                return parameters.CalculateValue(point); //Лежит ли точка ДОБАВИТЬ (bool) + 
             }
             
-
         }
         public class Parameters
         {
-            //методы преобразования коэффициентов путем поворота, перемещения и/или масштабирования
+            //Методы преобразования коэффициентов путем поворота, перемещения и/или масштабирования
+            public double aCoefficient;
+            public double bCoefficient;
+            public double cCoefficient;
+        
+            public Parameters(double a, double b, double c)
+            {
+                this.aCoefficient = a;
+                this.bCoefficient = b;
+                this.cCoefficient = c;
+            }
+            public void Rotate(double angle)
+            {
+                // Преобразовать коэффициенты уравнения при помощи поворота
+            }
+
+            public void Move(double x, double y, double z)
+            {
+                // Преобразовать коэффициенты уравнения при помощи перемещения
+            }
+
+            public void Scale(double xFactor, double yFactor, double zFactor)
+            {
+                // Преобразовать коэффициенты уравнения при помощи масштабирования
+            }
+        }
+        public class ParametersPlane : Parameters
+        {
             public double a;
             public double b;
             public double c;
-            public Parameters(double a, double b, double c)
+            public double d;   
+            public ParametersPlane()
             {
-                this.a = a;
-                this.b = b;
-                this.c = c;
+                a = 0;
+                b = 0;
+                c = 1;
+                d = 0;
+            }
+            public double CalculateValue(point pt)
+            {
+                return a * pt.x + b * pt.y + c * pt.z + d;
+            }
+            public double CalculateValue(Vector v)
+            {
+                return a * v.pt.x + b * v.pt.y + c * v.pt.z;
             }
         }
         public class BoundedPlane : Plane //  Главная плоскость
@@ -215,21 +302,35 @@ namespace Elementary_classes_engine
                 this.Delta_U = Delta_U;
                 this.Delta_V = Delta_V;
             }
-            public static bool InBoundaries(point Pt)  // Проверка координат точки на соответствие границам плоскости
+            public abstract bool InBoundaries(point Pt)  // Проверка координат точки на соответствие границам плоскости
             {
-                return true;
+           
             }
 
         }
 
+
         public class Sphere : Object
         {
-            public Sphere(point Position, Vector Rotation) : base(Position, Rotation){}
-            public static void contains(point Pt)
+            
+            private double Radius;
+            private Parameters parameters;
+
+
+            public Sphere(point Center, double Radius)
             {
-                // ДОБАВИТЬ (bool)
+                Position = Center;
+                this.Radius = Radius;
+                parameters = new Parameters;
             }
-            public static void intersect(Vector V)
+            
+            public override bool Contains(point Pt)
+            {
+                return Math.Pow(Pt.x - Position.x, 2) + Math.Pow(Pt.y - Position.y, 2) + Math.Pow(Pt.z - Position.z, 2) == Math.Pow(Radius, 2);
+                //Лежит ли точка на поверхности сферы 
+            }
+           
+            public intersect(Vector V)
             {
                 //  ДОБАВИТЬ
             }
@@ -238,7 +339,45 @@ namespace Elementary_classes_engine
                 // Радиус
             }
         }
-        
+        public class Map
+        {
+            private List<T> arrObjects;
+            public Map()
+            {
+
+                arrObjects = new List<T>();
+            }
+            public void Append(T obj)
+            {
+                arrObjects.Add(obj);
+            }
+            public void Remove(T obj)
+            {
+                arrObjects.Remove(obj);
+            }
+
+        }
+        public class Ray
+        {
+            point position;
+            Vector direction;
+            public Ray(point position, Vector direction) 
+            {
+                this.position = position;
+                this.direction = direction;
+            }
+            public override point Intersect(Map); //метод поиска ближайшей точки пересечения с объектами на карте
+            
+
+        }
+        public abstract class Canavas
+        {
+
+        }
+
+
+
+
 
 
     }
